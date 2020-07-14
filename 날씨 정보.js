acd6b3c51c1fe65c8267772d4404c2bd5a1737b5
Dark Tornado <dt3141592@naver.com>
2020-07-15 04:58:54 +0900

    Update 날씨 정보.js

--- 날씨 정보.js
+++ 날씨 정보.js
@@ -1,19 +1,25 @@
 /*
 날씨 정보
 © 2020 Dark Tornado, All rights reserved.
-Jsoup 라이브러리를 포함하고 있는 카톡봇에서 돌아가며, Utils 객체가 없다면 알아서 만드세요(?).
-라이선스는 GPL 3.0이며, 영리 목적으로 사용을 금합니다.
-날씨 정보 Copyright (C) 2020 Dark Tornado
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-This program is distributed in the hope that it will be useful,
+Jsoup 라이브러리를 포함하고 있는 봇 구동 앱에서 돌아가며, Utils 객체가 없다면 알아서 만드세요(?).
+라이선스는 LGPL 3.0이며, 영리 목적으로 사용을 금합니다.
+
+one line to give the library's name and an idea of what it does.
+Copyright (C) 2020  Dark Tornado
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 Utils.getWeather = function(pos) {
