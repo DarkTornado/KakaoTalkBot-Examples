7f09f60ba464ab1708f2b5bb9b214765ec703ed3
Dark Tornado <dt3141592@naver.com>
2020-07-15 21:49:51 +0900

    Update 자동 읽음 처리.js

--- 자동 읽음 처리.js
+++ 자동 읽음 처리.js
@@ -5,13 +5,20 @@
 
 var chatCount = {};
 
-function response(room, msg, sender, isGroupChat, replier, ImageDB) {
+function response(room, msg, sender, isGroupChat, _replier, ImageDB) {
+    var replier = {};
+    replier.reply = function(msg) {
+        _replier.reply(msg);
+        chatCount[room] = 0;
+    };
+
     if (chatCount[room] == undefined) chatCount[room] = 0;
     chatCount[room]++;
+
     if (chatCount[room] >= 300) {
         replier.reply("채팅이 300개 쌓여 자동 읽음 처리합니다.");
         chatCount[room] = 0;
     }
+    
+    //나머지 소스
 }
-
-
